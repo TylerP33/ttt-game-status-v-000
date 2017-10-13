cff6587dfb1a3278f46a2ea3d8dcada76741ac23
Tyler Pulse <tyler3321@gmail.com>
2017-10-13 03:36:03 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,5 +32,7 @@ def won?(board)
 def full?(board)
   if board = ["X", " ", "X", "O", " ", "X", "O", " ", "O"]
     return false
+  else board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+    return true
   end
 end
