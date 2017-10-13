5bc00247a542fd9b1b85829b3f183473d74e1e5b
Tyler Pulse <tyler3321@gmail.com>
2017-10-13 02:21:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,17 +4,13 @@ def position_taken?(board, index)
 end
 
 WIN_COMBINATIONS = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], #horizonol - array 0
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], #combination[0]
     [0, 3, 6], [1, 4, 7], [2, 5, 8], #vertical - array 1
     [0, 4, 8], [2, 4, 6], #diagnol - array 2
 ]
 
 def won?(board)
   WIN_COMBINATIONS.detect do |combinations|
-   combinations[0] == board[combinations[0]]
-
-
-
-
- end
+   end
+   combination[0[combination[1]]]
 end
