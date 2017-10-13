aeace594c8c36dc363f4c297fe1862f61b969e3a
Tyler Pulse <tyler3321@gmail.com>
2017-10-13 02:56:49 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
 end
 
 WIN_COMBINATIONS = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], #combination[0]
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], #Top
     [0, 3, 6], [1, 4, 7], [2, 5, 8], #vertical - array 1
     [0, 4, 8], [2, 4, 6], #diagnol - array 2
 ]
@@ -12,6 +12,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.detect do |combinations|
     combinations[0]
+    combinations[0][1]
 
 
    end
