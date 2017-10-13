f1bd9afe535cd290b305cec2efaaa02221db40ad
Tyler Pulse <tyler3321@gmail.com>
2017-10-13 03:07:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -11,9 +11,9 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.detect do |combinations|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
+    win_index_1 = combinations[0]
+    win_index_2 = combinations[1]
+    win_index_3 = combinations[2]
 
       position_1 = board[win_index_1]
       position_2 = board[win_index_2]
