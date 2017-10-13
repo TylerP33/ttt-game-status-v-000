18ba9d6fdbabe38ca13cb1139664735516070286
Tyler Pulse <tyler3321@gmail.com>
2017-10-13 04:18:37 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -42,3 +42,11 @@ def full?(board)
       return false
     end
   end
+
+  def over?(board)
+    if !won(board) && full?(board) && draw?(board)
+      return true
+    else
+      return false
+    end
+  end 
